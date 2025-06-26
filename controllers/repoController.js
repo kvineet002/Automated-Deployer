3887a2d75738fe35df6cfe6b34394b01da05be3d
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2025-06-26 20:18:58 +0530

    feat: Implement containerization feature for deploying repositories with Docker

--- controllers/repoController.js
+++ controllers/repoController.js
@@ -34,3 +34,30 @@ export const handleRepoSubmit = async (req, res) => {
         res.render('form', { error: 'Failed to detect stack.' });
     }
 };
+
+import path from 'path';
+import { exec } from 'child_process';
+
+export const handleContainerization = async (req, res) => {
+    const { repo } = req.body;
+    if (!repo) return res.status(400).send('No repo provided.');
+
+    const repoName = repo.split('/').pop().replace('.git', '');
+    const tempPath = path.join('./cloned_repos', repoName);
+    const composePath = path.join(tempPath, 'docker-compose.yml');
+
+    try {
+        exec(`docker compose -f ${composePath} up -d --build`, (err, stdout, stderr) => {
+            if (err) {
+                console.error('Docker error:', stderr);
+                return res.status(500).send('❌ Failed to deploy.');
+            }
+
+            console.log('Docker deployed successfully:', stdout);
+            res.send(`✅ Deployment started for ${repoName}!<br><a href="/">Go Back</a>`);
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('❌ Deployment failed.');
+    }
+};
--- routes/repoRoutes.js
+++ routes/repoRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { showForm, handleRepoSubmit } from '../controllers/repoController.js';
+import { showForm, handleRepoSubmit, handleContainerization } from '../controllers/repoController.js';
 
 const router = express.Router();
 
 router.get('/', showForm);
 router.post('/detect', handleRepoSubmit);
-
+router.post('/deploy', handleContainerization   );
 export default router;
